fb1c005382e3e38fc7c292c443dad8ce11b830a2
David Vollbracht <david.vollbracht@gmail.com>
2011-06-11 17:25:22 -0400

    OSX App bundle building

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,17 @@
+import Distribution.MacOSX
 import Distribution.Simple
-main = defaultMain
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks {
+         postBuild = appBundleBuildHook guiApps -- no-op if not MacOS X
+       }
+
+guiApps :: [MacApp]
+guiApps = [MacApp "hs-snake"
+                  Nothing --(Just "resources/WxHello.icns")
+                  Nothing -- Build a default Info.plist for the icon.
+                  [] -- No other resources.
+                  [] -- No other binaries.
+                  DoNotChase -- Try changing to ChaseWithDefaults
+          ]
+
